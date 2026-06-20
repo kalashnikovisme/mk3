e764b81d6e09613d9080c7563255e8f10615044d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2026-06-20 19:04:11 +0400

    updates

--- lib/fighting_ai/runtime/frame_capture_runner.rb
+++ lib/fighting_ai/runtime/frame_capture_runner.rb
@@ -4,6 +4,7 @@ module FightingAI
       INITIAL_FRAME_NUMBER = 1
       FRAME_INCREMENT      = 1
       FIELD_SEPARATOR      = "; "
+      MS_PER_SECOND        = 1000
 
       def initialize(emulator:, output:, frame_limit:, frame_advancer: nil, metadata_detector: nil)
         @emulator       = emulator
@@ -21,11 +22,14 @@ module FightingAI
 
         while @running && frame_number <= @frame_limit
           @frame_advancer.call
+          t0 = Process.clock_gettime(Process::CLOCK_MONOTONIC)
           frame = capture_frame
           break unless frame
 
           fields = { f: frame_number, size: File.size(frame.path) }
           fields.merge!(@metadata_detector.call(frame)) if @metadata_detector
+          elapsed_ms = ((Process.clock_gettime(Process::CLOCK_MONOTONIC) - t0) * MS_PER_SECOND).round
+          fields[:ms] = elapsed_ms
           @output.puts(fields.map { |name, value| "#{name}: #{value}" }.join(FIELD_SEPARATOR))
           frame_number += FRAME_INCREMENT
         end
